7e1674ad06c6fb1dcd2f40df0aa307339520d5c3
Estiaan Janse Van Rensburg <xero416@gmail.com>
2021-01-06 20:34:02 +1100

    Changed Ship to be a extension of RadarObject

--- src/entities/RadarObject.scala
+++ src/entities/RadarObject.scala
@@ -1,7 +1,54 @@
 package entities
 
+import gamemath.Raycasting.Boundary
 import gamemath.VectorD
 
-class RadarObject (var pos: VectorD, var radius: Double, var radarOpaccity: Double){
+import java.util
+
+class RadarObject (val width: Double, val height: Double, val MOI: Double, val mass: Double){
+  protected var pos = VectorD(0,0)
+  protected var velocity = VectorD(0,0)
+  protected var acceleration = VectorD(0,0)
+  protected var angularAcceleration = 0.0
+  protected var angularVelocity = 0.0
+  protected var rotation = 0.0
+
+  def setPos(newPos: VectorD): Unit ={
+    pos = VectorD(newPos.x,newPos.y)
+  }
+
+  def impulse(vector: VectorD): Unit ={
+    velocity = velocity + VectorD(vector.x,vector.y)
+  }
+
+  def rotatePoint(point: VectorD, centerX: Double, centerY: Double): VectorD ={
+    val tempX = point.x - centerX
+    val tempY = point.y - centerY
+
+    val rotatedX: Double = tempX * Math.cos(this.rotation) - tempY * Math.sin(this.rotation)
+    val rotatedY: Double = tempX * Math.sin(this.rotation) - tempY * Math.cos(this.rotation)
+
+    VectorD(rotatedX + centerX, rotatedY + centerY)
+  }
+
+  def getBoundaries(): util.ArrayList[Boundary] = {
+    val a = rotatePoint((VectorD(pos.x+0.5*width,pos.y-0.5*height)),pos.x,pos.y)
+    val b = rotatePoint((VectorD(pos.x+0.5*width,pos.y+0.5*height)),pos.x,pos.y)
+    val c = rotatePoint((VectorD(pos.x-0.5*width,pos.y+0.5*height)),pos.x,pos.y)
+    val d = rotatePoint((VectorD(pos.x-0.5*width,pos.y-0.5*height)),pos.x,pos.y)
+
+    val boundAB = new Boundary(a,b)
+    val boundBC = new Boundary(b,c)
+    val boundCD = new Boundary(c,d)
+    val boundDA = new Boundary(d,a)
+
+    var bList: util.ArrayList[Boundary] = new util.ArrayList()
+    bList.add(boundAB)
+    bList.add(boundBC)
+    bList.add(boundCD)
+    bList.add(boundDA)
+
+    return bList
+  }
 
 }
--- src/entities/Ship.scala
+++ src/entities/Ship.scala
@@ -7,62 +7,19 @@ import processing.core.PApplet
 
 import java.util
 
-class Ship(val width: Double, val height: Double, val MOI: Double, val mass: Double, val graphic: ShipGraphic) {
+class Ship(override val width: Double,override  val height: Double,override  val MOI: Double,override  val mass: Double, val graphic: ShipGraphic) extends
+  RadarObject (width: Double,height: Double, MOI: Double, mass: Double){
   private val integ = Integrator
   private var torque = 0.0
   private var netForce = VectorD(0,0)
-  private var pos = VectorD(0,0)
-  private var velocity = VectorD(0,0)
-  private var acceleration = VectorD(0,0)
-  private var angularAcceleration = 0.0
-  private var angularVelocity = 0.0
-  private var rotation = 0.0
   private val radarEmitter = new RadarEmitter()
-
-  def setPos(newPos: VectorD): Unit ={
-    pos = VectorD(newPos.x,newPos.y)
-  }
 
   def moveTo(mx: Float, my: Float): Unit ={
     impulse(new VectorD(mx,my) - pos)
   }
 
-  def impulse(vector: VectorD): Unit ={
-    velocity = velocity + VectorD(vector.x,vector.y)
-  }
-
   def rotationImpulse(rot: Double): Unit ={
     rotation = rotation + rot
-  }
-
-  def getBoundaries(): util.ArrayList[Boundary] = {
-    val a = rotatePoint((VectorD(pos.x+0.5*width,pos.y-0.5*height)),pos.x,pos.y)
-    val b = rotatePoint((VectorD(pos.x+0.5*width,pos.y+0.5*height)),pos.x,pos.y)
-    val c = rotatePoint((VectorD(pos.x-0.5*width,pos.y+0.5*height)),pos.x,pos.y)
-    val d = rotatePoint((VectorD(pos.x-0.5*width,pos.y-0.5*height)),pos.x,pos.y)
-
-    val boundAB = new Boundary(a,b)
-    val boundBC = new Boundary(b,c)
-    val boundCD = new Boundary(c,d)
-    val boundDA = new Boundary(d,a)
-
-    var bList: util.ArrayList[Boundary] = new util.ArrayList()
-    bList.add(boundAB)
-    bList.add(boundBC)
-    bList.add(boundCD)
-    bList.add(boundDA)
-
-    return bList
-  }
-
-  def rotatePoint(point: VectorD, centerX: Double, centerY: Double): VectorD ={
-    val tempX = point.x - centerX
-    val tempY = point.y - centerY
-
-    val rotatedX: Double = tempX * Math.cos(this.rotation) - tempY * Math.sin(this.rotation)
-    val rotatedY: Double = tempX * Math.sin(this.rotation) - tempY * Math.cos(this.rotation)
-
-    VectorD(rotatedX + centerX, rotatedY + centerY)
   }
 
   def tick(radarObjectList: util.ArrayList[Ship],context: PApplet): Unit ={
@@ -84,6 +41,4 @@ class Ship(val width: Double, val height: Double, val MOI: Double, val mass: Dou
     graphic.draw(context)
     //radarEmitter.draw(context)
   }
-
-
 }
